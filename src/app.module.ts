32370f9877f21e56a8088a99b2f1d0986bd2909f
Ida <ida.naderian@google.com>
2024-01-22 06:48:12 +0100

    connect to atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { EnvironmentModule } from './environment/environment.module';
 import { AutomationModule } from './automation/automation.module';
 
@@ -16,12 +16,21 @@ import { AutomationModule } from './automation/automation.module';
         limit: 10,
       },
     ]),
-    MongooseModule.forRoot(process.env.DATABASE_URI, {
-      dbName: process.env.DATABASE_NAME,
-      auth: {
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASS,
-      },
+    //connect to docker mongodb
+    // MongooseModule.forRoot(process.env.DATABASE_URI, {
+    //   dbName: process.env.DATABASE_NAME,
+    //   auth: {
+    //     username: process.env.DATABASE_USER,
+    //     password: process.env.DATABASE_PASS,
+    //   },
+    // }),
+    //connect to mongodb atlas
+    MongooseModule.forRootAsync({
+      useFactory: (): MongooseModuleOptions => ({
+        uri: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URI}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`,
+        retryAttempts: 6,
+        retryDelay: 1000,
+      }),
     }),
     EnvironmentModule,
     AutomationModule,
